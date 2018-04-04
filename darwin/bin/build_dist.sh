56d651380db7ca189c4df9410b9f074418a0be60
Felipe Castillo <fcastillo.ec@gmail.com>
2018-04-03 20:30:59 -0700

    Fix for darwin dist script

--- darwin/bin/build_dist.sh
+++ darwin/bin/build_dist.sh
@@ -5,7 +5,7 @@ set -e
 echo -e '\n\033[1mPackaging dmg installer: \033[01;0m'
 mkdir -p build/installers
 zip -ryq "Headset-$NEW_VERSION.zip" build/Headset-darwin-x64/Headset.app
-create-dmg build/Headset-darwin-x64/Headset.app
+set +e; create-dmg build/Headset-darwin-x64/Headset.app; set -e
 
 mv "Headset-$NEW_VERSION.dmg" build/installers/.
 mv "Headset-$NEW_VERSION.zip" build/installers/.
